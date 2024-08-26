eb7c013bf4cf8f4290d6a8310ef670a1e12160c1
Arinx <91897119+arinp95@users.noreply.github.com>
2024-08-26 13:59:50 +0530

    main.py

--- main.py
+++ main.py
@@ -4,58 +4,64 @@ import pickle
 import requests
 import gdown
 
-url = "https://drive.google.com/file/d/12r4sxc8DUB10YN6-vJMYDvAmUqJdTHsN/view?usp=drive_link"
+# Correct Google Drive download link format for gdown
+url = "https://drive.google.com/uc?id=12r4sxc8DUB10YN6-vJMYDvAmUqJdTHsN"
 output = 'similarity.pkl'
 gdown.download(url, output, quiet=False)
 
 def fetch_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8bab2ea3bb054ae93a65c957b41c3988".format(movie_id))
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8bab2ea3bb054ae93a65c957b41c3988")
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
+    try:
+        movie_index = movies[movies['title'] == movie].index[0]
+    except IndexError:
+        return [], []  # Return empty lists if the movie is not found
+
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
     recommended_movies_posters = []
     for i in movies_list:
-
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch poster from API
         recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies,recommended_movies_posters
+    return recommended_movies, recommended_movies_posters
 
-
+# Load movie data and similarity matrix
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-
-# Now you can load the file
+# Load the similarity matrix
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
+# Streamlit UI
 st.title('Movie Recommender System')
 
-selected_movie_name = st.selectbox('how would you like to be contacted?', movies['title'].values)
+selected_movie_name = st.selectbox('Select a movie', movies['title'].values)
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie_name)
+    
+    if recommended_movie_names:
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(recommended_movie_names[0])
+            st.image(recommended_movie_posters[0])
+        with col2:
+            st.text(recommended_movie_names[1])
+            st.image(recommended_movie_posters[1])
+        with col3:
+            st.text(recommended_movie_names[2])
+            st.image(recommended_movie_posters[2])
+        with col4:
+            st.text(recommended_movie_names[3])
+            st.image(recommended_movie_posters[3])
+        with col5:
+            st.text(recommended_movie_names[4])
+            st.image(recommended_movie_posters[4])
+    else:
+        st.error("Sorry, we couldn't find the movie or recommendations.")
